bb4664dfe946f4246ae80e1a1962f57b7f61779e
Mauro Iorio <mio@bit4id.com>
2018-03-23 17:08:16 +0100

    [arch-iso] Added some testing for the new image

--- arch-iso/build.sh
+++ arch-iso/build.sh
@@ -15,3 +15,21 @@ docker build --build-arg ISO_VER=${VER} \
              -t ioriomauro/arch-iso:${VER} \
              -t ioriomauro/arch-iso:latest \
              .
+
+(
+    echo -e "\n\n\n"
+    echo "*************************************"
+    echo "*****     Testing new image     *****"
+    echo "*************************************"
+    echo -e "\n\n\n"
+
+    docker run --rm -ti ioriomauro/arch-iso:latest /usr/bin/bash -c "
+        set -ux -- && \
+        pacman --noconfirm -S sudo && \
+        useradd -m -G wheel -s /bin/bash developer && \
+        echo '%wheel ALL=(ALL) NOPASSWD: ALL' >/etc/sudoers.d/developer && \
+        if [ \$(su -lc 'sudo whoami' developer) != 'root' ]; then
+            exit 1;
+        fi
+    "
+)
