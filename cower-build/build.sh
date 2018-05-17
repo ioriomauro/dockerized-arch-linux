b1d19c05cb467eac87d42d95f41ee9d808b3758f
Mauro Iorio <mio@bit4id.com>
2018-05-17 19:21:36 +0200

    [cower-build] Added some testing to the image build script

--- cower-build/build.sh
+++ cower-build/build.sh
@@ -13,3 +13,20 @@ trap atexit EXIT
 pushd "$BASEDIR" >/dev/null 2>/dev/null
 
 docker build -t ioriomauro/arch-cower-build:latest .
+
+(
+    echo -e "\n\n\n"
+    echo "*************************************"
+    echo "*****     Testing new image     *****"
+    echo "*************************************"
+    echo -e "\n\n\n"
+
+    docker run  --rm -ti \
+                -e UID=`id -ru` \
+                -e GID=`id -rg` \
+                ioriomauro/arch-cower-build:latest \
+                    tmuxinator \
+                    libcurl-openssl-1.0 \
+                    perl \
+                    pacaur
+)
