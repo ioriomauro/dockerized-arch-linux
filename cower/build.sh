050a70eff0a57d3b406862a216ece385b98a7978
Mauro Iorio <mio@bit4id.com>
2018-05-17 13:12:39 +0200

    [cower] Adding some image testing to build script

--- cower/build.sh
+++ cower/build.sh
@@ -13,3 +13,15 @@ trap atexit EXIT
 pushd "$BASEDIR" >/dev/null 2>/dev/null
 
 docker build -t ioriomauro/arch-cower:latest .
+
+(
+    echo -e "\n\n\n"
+    echo "*************************************"
+    echo "*****     Testing new image     *****"
+    echo "*************************************"
+    echo -e "\n\n\n"
+
+    set -e
+    docker run --rm -ti ioriomauro/arch-cower:latest -vs cower
+    docker run --rm -ti ioriomauro/arch-cower:latest -V
+)
