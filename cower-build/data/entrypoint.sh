3f3384813d9e682c0d6c8712e82dd901669f13be
Mauro Iorio <mio@bit4id.com>
2018-09-24 12:20:34 +0200

    [cower-build] Base packages were not upgraded due to a missing "-u" in pacman; added support for IGNORE_PACMAN_MIRRORS var

--- cower-build/data/entrypoint.sh
+++ cower-build/data/entrypoint.sh
@@ -2,8 +2,14 @@
 set -ue
 
 ARGS="$@"
+PAC_ARGS=""
+IGNORE_PACMAN_MIRRORS=${IGNORE_PACMAN_MIRRORS:-0}
+if [ "$IGNORE_PACMAN_MIRRORS" -eq "1" ]; then
+    PAC_ARGS="--ignore pacman-mirrorlist"
+fi
 
-sudo pacman -Syy --noconfirm
+
+sudo pacman -Syyu $PAC_ARGS --noconfirm
 /usr/bin/bash -l /home/developer/build-pkgs.sh ${ARGS}
 
 find /home/developer -iname "*.pkg.tar.xz" -exec sudo cp -v {} /packages/ \;
